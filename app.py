bd4c2485a07b9b13519dfc8fbd8b5a92530f7ac6
Gurek Singh <gurek15033@iiitd.ac.in>
2018-06-19 05:13:09 +0530

    app.py - Fix templatenotfound bug

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from uuid import uuid4
+import os
 
-app = Flask('__name__', template_folder='templates')
+app = Flask('__name__', template_folder=os.path.dirname(os.path.realpath(__file__))+'/templates/')
 app.config['SECRET_KEY'] = str(uuid4())
 bootstrap = Bootstrap(app)
 
