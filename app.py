25a5e7ff3011e83365c0f97eec1ed9b9e661d5f4
Gurek Singh <gurek15033@iiitd.ac.in>
2018-06-19 21:08:59 +0530

    style.css, base.html, static/ - completed footer

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8000, debug = True)
+	app.run(host = '0.0.0.0', port = 8000, debug = True)
